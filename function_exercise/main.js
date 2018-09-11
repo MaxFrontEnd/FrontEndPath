d29d78b55d0f2571ab39ab481c96e6f9deabe121
Max <maxidromlordus@mail.ru>
2018-09-11 18:24:43 +0300

    add some new functions

--- function_exercise/main.js
+++ function_exercise/main.js
@@ -38,7 +38,7 @@ function liftf(binary) {
   return function(first) {
     return function(second) {
       return binary(first, second);
-    }();
+    };
   };
 }
 
@@ -50,7 +50,51 @@ function curry(binary, first) {
   };
 }
 
+var inc = addf(1)
+var inc = liftf(add)(2);
+inc = curry(addf, 1);
 
-var inc = liftf(addf)(1);
 
-console.log(inc(5));
+function twice(binary) {
+  return function(a) {
+    return binary(a, a);
+  };
+}
+
+var doubl = twice(add);
+var square = twice(mul);
+
+function reverce(binary) {
+  return function(first, second) {
+    return binary(second, first);
+  };
+}
+
+function composeu(unary1, unary2) {
+  return function (third) {
+    return unary2(unary1(third));
+  };
+}
+
+
+function composeub(binary1, binary2) {
+  return function(third, fours, fives) {
+    return binary2(binary1(third, fours), fives);
+  };
+}
+var res = composeub(add, mul)(2, 3, 7);
+
+function limit(binary, times) {
+  return function(arg1, arg2) {
+    if (times > 0) {
+      times -= 1;
+      return binary(arg1, arg2);
+    }
+    return undefined;
+  };
+}
+
+var add_ltd = limit(add, 1);
+
+console.log(add_ltd(1, 2));
+console.log(add_ltd(1, 2));
